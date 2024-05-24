0a04347a049327361416c49f5bdabaeba53cfb3b
daniilsjb <daniilb@outlook.com>
2024-05-24 22:31:25 +0300

    Revert implementation of the MainLayout class

--- src/main/kotlin/lv/tsi/bookstore/common/MainLayout.kt
+++ src/main/kotlin/lv/tsi/bookstore/common/MainLayout.kt
@@ -3,7 +3,6 @@ package lv.tsi.bookstore.common
 import com.vaadin.flow.component.applayout.AppLayout
 import com.vaadin.flow.component.applayout.DrawerToggle
 import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.FlexComponent
@@ -18,82 +17,53 @@ import lv.tsi.bookstore.feature.user.Role
 import lv.tsi.bookstore.feature.user.UserView
 import lv.tsi.bookstore.feature.user.hasRole
 
-/*
-    MainLayout:
-        WMC: 1
-        CBO: 3
-        RFC: 5
-        LCOM4: 1
-
-    Header:
-        WMC: 1
-        CBO: 5
-        RFC: 10
-        LCOM4: 1
-
-    Drawer:
-        WMC: 1
-        CBO: 6
-        RFC: 8
-        LCOM4: 1
-*/
-
 class MainLayout(
-    securityService: SecurityService,
+    private val securityService: SecurityService,
 ) : AppLayout() {
 
+    // Initialize the application's header.
     init {
-        addToNavbar(Header(securityService))
-        addToDrawer(Drawer(securityService))
-        primarySection = Section.DRAWER
-    }
-}
-
-private class Header(securityService: SecurityService) : HorizontalLayout() {
-
-    init {
-        val logo = H1("Bookstore IMS").apply {
-            addClassNames(
-                LumoUtility.FontSize.LARGE,
-                LumoUtility.Margin.MEDIUM,
-            )
-        }
+        val logo = H1("Bookstore IMS")
+        logo.addClassNames(
+            LumoUtility.FontSize.LARGE,
+            LumoUtility.Margin.MEDIUM,
+        )
 
         val logout = Button("Log Out") {
             securityService.logout()
         }
 
-        add(DrawerToggle(), logo, logout)
-
-        expand(logo)
-        setWidthFull()
-
-        defaultVerticalComponentAlignment = FlexComponent.Alignment.CENTER
-        addClassNames(
+        val header = HorizontalLayout(DrawerToggle(), logo, logout)
+        header.defaultVerticalComponentAlignment = FlexComponent.Alignment.CENTER
+        header.expand(logo)
+        header.setWidthFull()
+        header.addClassNames(
             LumoUtility.Padding.Vertical.NONE,
             LumoUtility.Padding.Horizontal.MEDIUM,
         )
+
+        addToNavbar(header)
     }
-}
 
-private class Drawer(securityService: SecurityService) : Div() {
-
+    // Initialize the application's drawer.
     init {
         val hasManagerAccess = securityService
             .getAuthenticatedUserDetails()
             .hasRole(Role.MANAGER)
 
-        add(SideNav().apply {
+        addToDrawer(SideNav().apply {
             label = "Bookstore"
             addItem(SideNavItem("Inventory", BookView::class.java, VaadinIcon.DATABASE.create()))
             addItem(SideNavItem("Audit", AuditView::class.java, VaadinIcon.ARCHIVE.create()))
         })
 
         if (hasManagerAccess) {
-            add(SideNav().apply {
+            addToDrawer(SideNav().apply {
                 label = "Management"
                 addItem(SideNavItem("Users", UserView::class.java, VaadinIcon.USERS.create()))
             })
         }
+
+        primarySection = Section.DRAWER
     }
 }
